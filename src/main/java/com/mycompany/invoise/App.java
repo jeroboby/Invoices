535c433c3beae78261a78b5107fb04d1f66ecd46
jeroboby <jerome.mmetge@gmail.com>
2020-12-14 19:02:46 +0100

    Mise en place de springBoot

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -1,30 +1,22 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
-import com.mycompany.invoise.service.InvoiceServiceInterface;
-import com.mycompany.invoise.service.prefix.InvoiceServicePrefix;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
 
 /**
  * Hello world!
  *
  */
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.invoise.controller.web"/*,"com.mycompany.invoise.service.prefix"*/, "com.mycompany.invoise.repository.database"})
-@PropertySource("classpath:application.properties")
-
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class, args);
         InvoiceControllerInterface invoiceController = context.getBean(InvoiceControllerInterface.class);
         invoiceController.createInvoice();
     }
 
-    @Bean
-    public InvoiceServiceInterface configureInvoiceService() {
-        return new InvoiceServicePrefix();
-    }
 }
